12b275210ef8041c6fa7cacbb208bf7db360df0a
Ivan Weiler <ivan@favicode.net>
2024-02-22 14:01:22 +0100

    Cleanup, installments

--- monri.php
+++ monri.php
@@ -19,22 +19,23 @@ require_once __DIR__ . '/includes/logger.php';
 require_once __DIR__ . '/util.php';
 
 function monri_wc_init() {
-    if (!class_exists('WC_Payment_Gateway')) {
-	    return;
-    }
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
+		return;
+	}
 
 	require_once __DIR__ . '/includes/gateway.php';
 
-    function woocommerce_add_monri_gateway($methods) {
-        $methods[] = Monri_WC_Gateway::class;
-        return $methods;
-    }
-	add_filter('woocommerce_payment_gateways', 'woocommerce_add_monri_gateway');
+	function woocommerce_add_monri_gateway( $methods ) {
+		$methods[] = Monri_WC_Gateway::class;
+
+		return $methods;
+	}
+
+	add_filter( 'woocommerce_payment_gateways', 'woocommerce_add_monri_gateway' );
 
 	// woocommerce actions/filters here?!
 }
-add_action('plugins_loaded', 'monri_wc_init', 0);
-add_action('plugins_loaded', 'load_language');
+add_action( 'plugins_loaded', 'monri_wc_init', 0 );
 
 
 /**
@@ -43,25 +44,33 @@ add_action('plugins_loaded', 'load_language');
  */
 function monri_declare_woo_feature_compatibility() {
 	if ( class_exists( '\Automattic\WooCommerce\Utilities\FeaturesUtil' ) ) {
-		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility(
+			'custom_order_tables',
+			__FILE__,
+			true
+		);
 	}
 }
 //add_action( 'before_woocommerce_init', 'monri_declare_woo_feature_compatibility' );
 
 
-function monri_wc_action_links($links) {
+function monri_wc_action_links( $links ) {
 	$links[] = sprintf(
 		'<a href="%s">%s</a>',
-		admin_url('admin.php?page=wc-settings&tab=checkout&section=monri'),
-		__('Settings', 'monri')
+		admin_url( 'admin.php?page=wc-settings&tab=checkout&section=monri' ),
+		__( 'Settings', 'monri' )
 	);
-    return $links;
+
+	return $links;
 }
-add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'monri_wc_action_links');
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'monri_wc_action_links' );
+
 
 function load_language() {
-    load_plugin_textdomain( 'monri', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain( 'monri', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
+add_action( 'plugins_loaded', 'load_language' );
+
 
 // Registers Blocks integration.
 function monri_wc_block_support() {
@@ -69,13 +78,13 @@ function monri_wc_block_support() {
 		require_once __DIR__ . '/includes/blocks-support.php';
 		add_action(
 			'woocommerce_blocks_payment_method_type_registration',
-			function( Automattic\WooCommerce\Blocks\Payments\PaymentMethodRegistry $payment_method_registry ) {
+			function ( Automattic\WooCommerce\Blocks\Payments\PaymentMethodRegistry $payment_method_registry ) {
 				$payment_method_registry->register( new Monri_WC_Blocks_Support() );
 			}
 		);
 	}
 }
-add_action( 'woocommerce_blocks_loaded', 'monri_wc_block_support');
+add_action( 'woocommerce_blocks_loaded', 'monri_wc_block_support' );
 
 
 // controllers here? call what's needed
