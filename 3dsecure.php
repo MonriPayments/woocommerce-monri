a215e80999f6e658547ab0a595b14f6b1da27f4c
Ivan Weiler <ivan@favicode.net>
2024-02-29 13:59:10 +0100

    Escaping

--- 3dsecure.php
+++ 3dsecure.php
@@ -1,7 +1,10 @@
 <?php
-	if ( ! isset( $_GET['acsUrl'], $_GET['pareq'], $_GET['returnUrl'], $_GET['token'] ) ) {
-		return;
-	}
+
+require __DIR__ . '/../../../wp-load.php';
+
+if ( ! isset( $_GET['acsUrl'], $_GET['pareq'], $_GET['returnUrl'], $_GET['token'] ) ) {
+	return;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,11 +12,11 @@
         <title><?php echo 'Monri 3D Secure Verification' ?></title>
 	</head>
 	<body style="display:none">
-        <p><?php echo 'Invoking 3-D secure form, please wait ...'; ?></p>
-		<form id="3ds-redirect" name="form" action="<?php echo $_GET['acsUrl'] ?>" method="post">
-			<input  class="form-control" type="hidden" name="PaReq" value="<?php echo $_GET['pareq'] ?>">
-			<input  class="form-control" type="hidden" name="TermUrl" value="<?php echo $_GET['returnUrl'] ?>">
-			<input  class="form-control" type="hidden" name="MD" value="<?php echo $_GET['token'] ?>">
+        <p><?php esc_html_e( 'Invoking 3-D secure form, please wait ...', 'monri' ); ?></p>
+		<form id="3ds-redirect" name="form" action="<?php echo esc_url($_GET['acsUrl']) ?>" method="post">
+			<input  class="form-control" type="hidden" name="PaReq" value="<?php echo esc_attr( $_GET['pareq'] ) ?>">
+			<input  class="form-control" type="hidden" name="TermUrl" value="<?php echo esc_attr( $_GET['returnUrl'] ) ?>">
+			<input  class="form-control" type="hidden" name="MD" value="<?php echo esc_attr( $_GET['token'] ) ?>">
 
 			<noscript>
 				<input type="submit"/>
