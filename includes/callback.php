d4f09f6a846217748893bdb92e4fd8355087457b
David Runtic <osdavidruntic@gmail.com>
2024-11-20 14:34:26 +0100

    refactoring

--- includes/callback.php
+++ includes/callback.php
@@ -3,20 +3,20 @@
 class Monri_WC_Callback {
 
 	public function init() {
-		add_action( 'woocommerce_api_monri_callback', [ $this, 'handle_callback' ] );
+		add_action( 'woocommerce_api_monri_callback', array( $this, 'handle_callback' ) );
 	}
 
 	/**
 	 * Prints the error message and exits the process with a given HTTP Status Code.
 	 *
 	 * @param $message
-	 * @param array $status
+	 * @param array   $status
 	 */
 	private function error( $message, array $status = array() ) {
-		status_header($status[0], $status[1]);
+		status_header( $status[0], $status[1] );
 		header( 'Content-Type: text/plain' );
 
-		echo esc_html($message);
+		echo esc_html( $message );
 		exit( (int) $status[0] );
 	}
 
@@ -39,16 +39,15 @@ class Monri_WC_Callback {
 	 * Handles the given URL `$callback` as the callback URL for Monri Payment Gateway.
 	 * This endpoint accepts only POST requests which have their payload in the PHP Input Stream.
 	 * The payload must be a valid JSON.
-	 *
 	 */
 	private function handle_monri_webpay_callback() {
 
 		if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) {
-			$this->error( 'Invalid request method.', [ 400, 'Bad Request' ] );
+			$this->error( 'Invalid request method.', array( 400, 'Bad Request' ) );
 		}
 
 		if ( empty( $_SERVER['HTTP_AUTHORIZATION'] ) ) {
-			$this->error( 'Authorization header missing.', [ 400, 'Bad Request' ] );
+			$this->error( 'Authorization header missing.', array( 400, 'Bad Request' ) );
 		}
 
 		$authorization = sanitize_text_field( $_SERVER['HTTP_AUTHORIZATION'] );
@@ -58,16 +57,16 @@ class Monri_WC_Callback {
 		$merchant_key = Monri_WC_Settings::instance()->get_option( 'monri_merchant_key' );
 
 		if ( empty( $merchant_key ) ) {
-			$this->error( 'Merchant key not provided.', [ 404, 'Not found' ] );
+			$this->error( 'Merchant key not provided.', array( 404, 'Not found' ) );
 		}
 
-		$bad_request_header = [ 400, 'Bad Request' ];
+		$bad_request_header = array( 400, 'Bad Request' );
 
 		// json post comming from Monri webhook, validated by header, not user input but trusted data
 		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 		$json = file_get_contents( 'php://input' );
 
-		Monri_WC_Logger::log( "Request data: " . sanitize_textarea_field( $json ), __METHOD__ );
+		Monri_WC_Logger::log( 'Request data: ' . sanitize_textarea_field( $json ), __METHOD__ );
 
 		// Calculating the digest...
 		$digest = hash( 'sha512', $merchant_key . $json );
@@ -80,7 +79,8 @@ class Monri_WC_Callback {
 		try {
 			$payload = json_decode( $json, true );
 		} catch ( \Throwable $e ) {
-			$this->error( 'Invalid request content', $bad_request_header );;
+			$this->error( 'Invalid request content', $bad_request_header );
+
 		}
 
 		if ( ! isset( $payload['order_number'] ) || ! isset( $payload['status'] ) ) {
@@ -88,8 +88,8 @@ class Monri_WC_Callback {
 		}
 
 		$order_number = $payload['order_number'];
-		if (Monri_WC_Settings::instance()->get_option( 'test_mode' )) {
-			$order_number = Monri_WC_Utils::resolve_real_order_id($order_number);
+		if ( Monri_WC_Settings::instance()->get_option( 'test_mode' ) ) {
+			$order_number = Monri_WC_Utils::resolve_real_order_id( $order_number );
 		}
 
 		try {
@@ -99,44 +99,43 @@ class Monri_WC_Callback {
 				return;
 			}
 
-            $valid_response_code = isset( $payload['response_code'] ) && $payload['response_code'] === "0000";
+			$valid_response_code = isset( $payload['response_code'] ) && $payload['response_code'] === '0000';
 
 			if ( $payload['status'] === 'approved' && $valid_response_code ) {
 				$order->payment_complete();
 			} else {
 				$order->update_status( 'cancelled' );
 			}
-
 		} catch ( \Exception $e ) {
 			$message = sprintf( 'Order ID: %s not found or does not exist.', $order_number );
 			$this->error( $message, array( 404, 'Not Found' ) );
 		}
-
 	}
 
 	/**
 	 * Handles Monri WSPay callback. Must be a POST request
 	 */
 	private function handle_monri_wspay_callback() {
-		$bad_request = [ 400, 'Bad Request' ];
+		$bad_request = array( 400, 'Bad Request' );
 		if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) {
 			$this->error( 'Invalid request method.', $bad_request );
 		}
 
 		$json = file_get_contents( 'php://input' );
-		Monri_WC_Logger::log( "Request data: " . sanitize_textarea_field( $json ), __METHOD__ );
+		Monri_WC_Logger::log( 'Request data: ' . sanitize_textarea_field( $json ), __METHOD__ );
 
 		try {
 			$payload = json_decode( $json, true );
 		} catch ( \Throwable $e ) {
-			$this->error( 'Invalid request content', $bad_request_header );;
+			$this->error( 'Invalid request content', $bad_request_header );
+
 		}
 
-		if ( !$this->validate_monri_wspay_callback($payload) ) {
+		if ( ! $this->validate_monri_wspay_callback( $payload ) ) {
 			$this->error( 'Invalid signature.', $bad_request );
 		}
 
-		if (!in_array($this->get_monri_wspay_callback_action($payload), ['Authorized', 'Completed'])) {
+		if ( ! in_array( $this->get_monri_wspay_callback_action( $payload ), array( 'Authorized', 'Completed' ) ) ) {
 			$this->error( 'Invalid action.', $bad_request );
 		}
 
@@ -145,8 +144,8 @@ class Monri_WC_Callback {
 		}
 
 		$order_number = sanitize_text_field( $payload['ShoppingCartID'] );
-		if (Monri_WC_Settings::instance()->get_option( 'test_mode' )) {
-			$order_number = Monri_WC_Utils::resolve_real_order_id($order_number);
+		if ( Monri_WC_Settings::instance()->get_option( 'test_mode' ) ) {
+			$order_number = Monri_WC_Utils::resolve_real_order_id( $order_number );
 		}
 
 		$order = wc_get_order( $order_number );
@@ -161,14 +160,13 @@ class Monri_WC_Callback {
 				return;
 			}
 
-			$valid_response_code = isset( $payload['ActionSuccess'] ) && $payload['ActionSuccess'] === "1";
+			$valid_response_code = isset( $payload['ActionSuccess'] ) && $payload['ActionSuccess'] === '1';
 
 			if ( $valid_response_code ) {
 				$order->payment_complete();
 			} else {
 				$order->update_status( 'cancelled' );
 			}
-
 		} catch ( \Exception $e ) {
 			$message = sprintf( 'Order ID: %s not found or does not exist.', $order_number );
 			$this->error( $message, array( 404, 'Not Found' ) );
@@ -177,43 +175,43 @@ class Monri_WC_Callback {
 
 	/**
 	 * Validate the Monri WSPay callback request
+	 *
 	 * @param string[] $payload
 	 *
 	 * @return bool
 	 */
-	private function validate_monri_wspay_callback($payload) {
-		$shopID = Monri_WC_Settings::instance()->get_option( 'monri_ws_pay_form_shop_id' );
-		$secretKey = Monri_WC_Settings::instance()->get_option( 'monri_ws_pay_form_secret' );
-		$actionSuccess = sanitize_text_field( $payload['ActionSuccess'] ?? '');
-		$approvalCode = sanitize_text_field( $payload['ApprovalCode'] ?? '');
-		$wspayOrderId = sanitize_text_field( $payload['WsPayOrderId'] ?? '');
+	private function validate_monri_wspay_callback( $payload ) {
+		$shop_id        = Monri_WC_Settings::instance()->get_option( 'monri_ws_pay_form_shop_id' );
+		$secret_key     = Monri_WC_Settings::instance()->get_option( 'monri_ws_pay_form_secret' );
+		$action_success = sanitize_text_field( $payload['ActionSuccess'] ?? '' );
+		$approval_code  = sanitize_text_field( $payload['ApprovalCode'] ?? '' );
+		$wspay_order_id = sanitize_text_field( $payload['WsPayOrderId'] ?? '' );
 
 		$signature =
-			$shopID . $secretKey .
-			$actionSuccess .
-			$approvalCode .
-			$secretKey . $shopID .
-			$approvalCode .
-			$wspayOrderId;
+			$shop_id . $secret_key .
+			$action_success .
+			$approval_code .
+			$secret_key . $shop_id .
+			$approval_code .
+			$wspay_order_id;
 
-		$payloadSignature = $payload['Signature'] ?? '';
+		$payload_signature = $payload['Signature'] ?? '';
 
-		return $payloadSignature === hash('sha512', $signature);
-
+		return $payload_signature === hash( 'sha512', $signature );
 	}
 
 	/**
 	 *  Get callback action name
+	 *
 	 * @param string[] $payload
 	 *
 	 * @return string
 	 */
-	private function get_monri_wspay_callback_action($payload)
-	{
-		$orderedActions = ['Refunded', 'Voided', 'Completed', 'Authorized'];
-		$result = 'Unknown';
-		foreach ($orderedActions as $action) {
-			if (isset($payload[$action]) && $payload[$action] === '1') {
+	private function get_monri_wspay_callback_action( $payload ) {
+		$ordered_actions = array( 'Refunded', 'Voided', 'Completed', 'Authorized' );
+		$result          = 'Unknown';
+		foreach ( $ordered_actions as $action ) {
+			if ( isset( $payload[ $action ] ) && $payload[ $action ] === '1' ) {
 				$result = $action;
 				break;
 			}
