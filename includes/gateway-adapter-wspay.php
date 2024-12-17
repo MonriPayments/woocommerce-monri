0cc2a92e081f3de75c98b2caa2e2836dd62d084a
David Runtic <osdavidruntic@gmail.com>
2024-12-17 14:05:47 +0100

    fix

--- includes/gateway-adapter-wspay.php
+++ includes/gateway-adapter-wspay.php
@@ -522,7 +522,7 @@ class Monri_WC_Gateway_Adapter_Wspay {
 		$order            = wc_get_order( $order_id );
 		$transaction_info = $order->get_meta( '_monri_transaction_info' );
 		$is_tokenization  = $order->get_meta( '_monri_order_token_used' );
-		$transaction_type = $order->get_meta( '_monri_wspay_transaction_type' );
+		$transaction_type = $order->get_meta( 'monri_wspay_transaction_type' );
 
 		if ( empty( $transaction_info ) || $transaction_type === 'purchase') {
 			return false;
