aa03c40b5ede5d75d33cef5c4f00aba71115f192
David Runtic <osdavidruntic@gmail.com>
2025-09-15 15:47:10 +0200

    refactoring, added logic if qr code can be rendered only once

--- includes/blocks-support-components-keks.php
+++ includes/blocks-support-components-keks.php
@@ -13,16 +13,21 @@ final class Monri_WC_Keks_Blocks_Support extends AbstractPaymentMethodType {
 	 * @var string
 	 */
 	protected $name = 'monri-components-keks';
-
+	/**
+	 * Initializes the payment method type.
+	 */
 	public function initialize() {
-		$this->settings = get_option( 'woocommerce_monri_settings', [] );
+		$this->settings = get_option( 'woocommerce_monri_settings', array() );
 
-		add_action('enqueue_block_editor_assets', function () {
-			$script_url = $this->get_setting( 'test_mode' ) ?
+		add_action(
+			'enqueue_block_editor_assets',
+			function () {
+				$script_url = $this->get_setting( 'test_mode' ) ?
 				Monri_WC_Gateway_Adapter_Webpay_Components::SCRIPT_ENDPOINT_TEST :
 				Monri_WC_Gateway_Adapter_Webpay_Components::SCRIPT_ENDPOINT;
-			wp_enqueue_script( 'monri-components-keks', $script_url, array(), MONRI_WC_VERSION );
-		});
+				wp_enqueue_script( 'monri-components-keks', $script_url, array(), MONRI_WC_VERSION );
+			}
+		);
 	}
 
 	/**
@@ -31,9 +36,9 @@ final class Monri_WC_Keks_Blocks_Support extends AbstractPaymentMethodType {
 	 * @return boolean
 	 */
 	public function is_active() {
-		$settings = get_option( 'woocommerce_monri_settings', [] );
-		$supported_payment_methods = $settings['monri_web_pay_supported_payment_methods'] ?? [];
-		if ( $settings['monri_web_pay_integration_type'] === 'components' && in_array( 'keks-pay-hr', $supported_payment_methods, true ) ) {
+		$settings                  = get_option( 'woocommerce_monri_settings', array() );
+		$supported_payment_methods = $settings['monri_web_pay_supported_payment_methods'] ?? array();
+		if ( $settings['monri_web_pay_integration_type'] === 'components' && is_array($supported_payment_methods) && in_array( 'keks-pay-hr', $supported_payment_methods ) ) {
 			return true;
 		}
 		return false;
@@ -45,19 +50,20 @@ final class Monri_WC_Keks_Blocks_Support extends AbstractPaymentMethodType {
 	 * @return array
 	 */
 	public function get_payment_method_script_handles() {
+		//todo: load separate script for keks
 		$script_path       = '/assets/js/blocks/index.js';
 		$script_asset_path = MONRI_WC_PLUGIN_PATH . 'assets/js/blocks/index.asset.php';
 		$script_asset      = file_exists( $script_asset_path )
-			? require( $script_asset_path )
+			? require $script_asset_path
 			: array(
 				'dependencies' => array(),
-				'version'      => MONRI_WC_VERSION
+				'version'      => MONRI_WC_VERSION,
 			);
-		$script_url = MONRI_WC_PLUGIN_URL . $script_path;
+		$script_url        = MONRI_WC_PLUGIN_URL . $script_path;
 
 		if ( $this->get_setting( 'monri_payment_gateway_service' ) === 'monri-web-pay' &&
-		     $this->get_setting( 'monri_web_pay_integration_type' ) === 'components' &&
-             wp_script_is( 'monri-components' )
+			$this->get_setting( 'monri_web_pay_integration_type' ) === 'components' &&
+			wp_script_is( 'monri-components' )
 		) {
 			$script_asset['dependencies'][] = 'monri-components';
 		}
@@ -73,7 +79,6 @@ final class Monri_WC_Keks_Blocks_Support extends AbstractPaymentMethodType {
 			wp_set_script_translations( 'monri-keks-blocks', 'monri', MONRI_WC_PLUGIN_PATH . 'languages/' );
 		}
 
-		return [ 'monri-keks-blocks' ];
+		return array( 'monri-keks-blocks' );
 	}
-
 }
--- includes/gateway-webpay-components-keks.php
+++ includes/gateway-webpay-components-keks.php
@@ -3,30 +3,40 @@ use Automattic\WooCommerce\StoreApi\Schemas\V1\CartSchema;
 
 class Monri_WC_Gateway_Webpay_Components_Keks extends WC_Payment_Gateway {
 	public const AUTHORIZATION_ENDPOINT_TEST = 'https://ipgtest.monri.com/v2/payment/new';
-	public const AUTHORIZATION_ENDPOINT = 'https://ipg.monri.com/v2/payment/new';
+	public const AUTHORIZATION_ENDPOINT      = 'https://ipg.monri.com/v2/payment/new';
 
 	/**
+	 * Supported features
+	 *
 	 * @var string[]
 	 */
-	public $supports = [ 'products', 'refunds' ];
+	public $supports = array( 'products', 'refunds' );
 
+	/**
+	 * Gateway ID
+	 *
+	 * @var string
+	 */
 	public $id = 'monri-components-keks';
 
 	/**
+	 * Components Keks constructor.
 	 *
 	 * @return void
 	 */
 	public function __construct() {
 		$this->init_settings();
-		$this->has_fields = false;
+		$this->has_fields  = false;
 		$this->title       = __( 'Monri Keks', 'monri' );
 		$this->description = __( 'Pay with Monri Keks', 'monri' );
 
-		add_action( 'woocommerce_receipt_' . $this->id, [ $this, 'process_components_keks' ] );
+		add_action( 'woocommerce_receipt_' . $this->id, array( $this, 'process_components_keks' ) );
 	}
 
 
 	/**
+	 * Process the payment and return the result
+	 *
 	 * @param int $order_id
 	 *
 	 * @return array
@@ -35,49 +45,64 @@ class Monri_WC_Gateway_Webpay_Components_Keks extends WC_Payment_Gateway {
 	public function process_payment( $order_id ) {
 
 		$order = wc_get_order( $order_id );
+		// QR code can be rendered only once per order?
+//		if ( $order->get_meta( 'keks_qr_code_rendered' ) === 'yes' ) {
+//			$order->set_status( 'cancelled', 'Order has been cancelled. Please go to checkout and try again.' );
+//		}
+//		$order->update_meta_data( 'keks_qr_code_rendered', 'yes' );
+//		$order->save();
 		return array(
 			'result'   => 'success',
-			'redirect' => $order->get_checkout_payment_url( true )
+			'redirect' => $order->get_checkout_payment_url( true ),
 		);
 	}
 
 	/**
+	 * Passes config data to template file
 	 *
-	 * @return string
+	 * @param $order_id
+	 *
+	 * @return void
 	 */
 	public function process_components_keks( $order_id ) {
 
-		wc_get_template( 'components-keks.php', [
-			'config' => [
-				'is_test'            => $this->get_option_bool( 'test_mode' ) ? 'test' : 'prod',
-				'client_secret'      => $this->request_authorize( wc_get_order( $order_id ) ),
-				'authenticity_token' => $this->get_option( 'monri_authenticity_token' ),
-				'locale'             => $this->get_option( 'form_language' ),
-			],
-		], basename( MONRI_WC_PLUGIN_PATH ), MONRI_WC_PLUGIN_PATH . 'templates/' );
-
+		wc_get_template(
+			'components-keks.php',
+			array(
+				'config' => array(
+					'is_test'            => $this->get_option_bool( 'test_mode' ) ? 'test' : 'prod',
+					'client_secret'      => $this->request_authorize( wc_get_order( $order_id ) ),
+					'authenticity_token' => $this->get_option( 'monri_authenticity_token' ),
+					'locale'             => $this->get_option( 'form_language' ),
+				),
+			),
+			basename( MONRI_WC_PLUGIN_PATH ),
+			MONRI_WC_PLUGIN_PATH . 'templates/'
+		);
 	}
 
-	private function request_authorize($order) {
-		$amount_in_minor_units = (int) round( $order->get_total() * 100 );
-		$session_client_secret = $this->get_session_client_secret( $amount_in_minor_units );
-		if ( ! empty( $session_client_secret ) ) {
-			return $session_client_secret;
-		}
-
+	/**
+	 * Generate client secret for Keks QR code
+	 *
+	 * @param WC_Order $order
+	 *
+	 * @return mixed
+	 */
+	private function request_authorize( $order ) {
 		$order_id = $order->get_id();
 		if ( $this->get_option_bool( 'test_mode' ) ) {
-			$order_id = Monri_WC_Utils::get_test_order_id( $order_id );;
+			$order_id = Monri_WC_Utils::get_test_order_id( $order_id );
+
 		}
 
 		$url = $this->get_option_bool( 'test_mode' ) ?
 			self::AUTHORIZATION_ENDPOINT_TEST :
 			self::AUTHORIZATION_ENDPOINT;
 
-		if ( is_admin() ) { // admin page editor
+		if ( is_admin() ) { // admin page editor.
 			$order_total = 10;
 		} else {
-			$order_total = (float) WC()->cart->get_total( 'edit' );
+			$order_total = (float) $order->get_total();
 		}
 
 		$amount_in_minor_units = (int) round( $order_total * 100 );
@@ -87,19 +112,19 @@ class Monri_WC_Gateway_Webpay_Components_Keks extends WC_Payment_Gateway {
 			$currency = 'BAM';
 		}
 
-		$data = [
+		$data = array(
 			'amount'           => $amount_in_minor_units,
 			'order_number'     => $order_id,
 			'currency'         => $currency,
 			'transaction_type' => $this->get_option_bool( 'transaction_type' ) ? 'authorize' : 'purchase',
-			'order_info'       => 'woocommerce order'
-		];
-
+			'order_info'       => 'woocommerce order',
+		);
 
 		$data = wp_json_encode( $data );
 
 		$timestamp = time();
-		$digest    = hash( 'sha512',
+		$digest    = hash(
+			'sha512',
 			$this->get_option( 'monri_merchant_key' ) .
 			$timestamp .
 			$this->get_option( 'monri_authenticity_token' ) .
@@ -110,36 +135,37 @@ class Monri_WC_Gateway_Webpay_Components_Keks extends WC_Payment_Gateway {
 
 		Monri_WC_Logger::log( $data, __METHOD__ );
 
-		$response = wp_remote_post( $url, [
+		$response = wp_remote_post(
+			$url,
+			array(
 				'body'      => $data,
-				'headers'   => [
+				'headers'   => array(
 					'Content-Type'   => 'application/json',
 					'Content-Length' => strlen( $data ),
-					'Authorization'  => $authorization
-				],
+					'Authorization'  => $authorization,
+				),
 				'timeout'   => 10,
-				'sslverify' => true
-			]
+				'sslverify' => true,
+			)
 		);
 
 		Monri_WC_Logger::log( $response, __METHOD__ );
 
 		if ( is_wp_error( $response ) ) {
-			$response = [ 'status' => 'error', 'error' => $response->get_error_message() ];
+			$response = array(
+				'status' => 'error',
+				'error'  => $response->get_error_message(),
+			);
 		}
 
-		$body          = wp_remote_retrieve_body( $response );
-		$client_secret = json_decode( $body, true )['client_secret'];
-		// save data to session so that we can reuse it on site refresh
-		WC()->session->set(
-			$amount_in_minor_units . '_client_secret_timestamp',
-			$amount_in_minor_units . '_' . $client_secret . '_' . time()
-		);
+		$body = wp_remote_retrieve_body( $response );
 
-		return $client_secret;
+		return json_decode( $body, true )['client_secret'];
 	}
 
 	/**
+	 * Gateway options getter
+	 *
 	 * @param string $key
 	 *
 	 * @return bool
@@ -148,45 +174,17 @@ class Monri_WC_Gateway_Webpay_Components_Keks extends WC_Payment_Gateway {
 		return in_array( $this->get_option( $key ), array( 'yes', '1', true ), true );
 	}
 
+	/**
+	 * Init settings for gateways.
+	 */
 	public function init_settings() {
-		$this->settings = get_option( 'woocommerce_monri_settings', [] );
+		$this->settings = get_option( 'woocommerce_monri_settings', array() );
 	}
 
 	/**
-	 * Return client secret from session if it is valid
-	 *
-	 * @param int $amount_in_minor_units
-	 *
-	 * @return string
+	 * Temporary solution for admin options until keks pay is completely independat of monri components
 	 */
-	public function get_session_client_secret( $amount_in_minor_units ) {
-		// @todo: find out exact time
-		$allowed_time_seconds           = 900;
-
-		$amount_client_secret_timestamp = WC()->session->get( (string) $amount_in_minor_units . '_client_secret_timestamp' );
-		if (empty($amount_client_secret_timestamp)) {
-			return null;
-		}
-
-		$amount_client_secret_timestamp = explode( '_', $amount_client_secret_timestamp );
-		if ( count( $amount_client_secret_timestamp ) !== 3 ) {
-			return null;
-		}
-
-		[ $amount, $client_secret, $timestamp ] = $amount_client_secret_timestamp;
-
-		if (
-			empty($amount) ||
-			empty($client_secret) ||
-			empty($timestamp) ||
-			(time() - $timestamp) > $allowed_time_seconds ||
-			(int) $amount !== $amount_in_minor_units
-		) {
-			return null;
-		}
-
-		return $client_secret;
-
+	public function admin_options() {
+		echo '<p>' . esc_html__( 'This payment method is managed by the main Monri Payments settings.', 'monri' ) . '</p>';
 	}
-
 }
