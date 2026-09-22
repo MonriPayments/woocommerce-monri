3cf8199a5cd8a51847faa6001aa7f7b2a801a462
David Runtic <osdavidruntic@gmail.com>
2026-09-22 10:30:43 +0200

    bump woo version

--- monri.php
+++ monri.php
@@ -8,7 +8,7 @@ Author URI: https://monri.com
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 WC requires at least: 4.3.0
-WC tested up to: 11.0.1
+WC tested up to: 11.1.1
 Text Domain: monri
 */
 if ( ! defined( 'ABSPATH' ) ) exit;
